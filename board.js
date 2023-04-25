caa6807b3c822ba9c47236a3b41fe59c67f4bdf9
DenisWithOneN <denis.ursut@emanuel.com>
2023-04-25 11:25:24 +0300

    Refference added + Canvas

--- board.js
+++ board.js
@@ -1,5 +1,7 @@
-<canvas>
-
-
-
-</canvas>
+function setup() {
+    canvas = createCanvas(580, 500);
+    background(100);
+    let canvasX = (windowWidth - width) / 2;
+    let canvasY = (windowHeight - height) / 2;
+    canvas.position(canvasX, canvasY);
+  }
