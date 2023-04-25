c9d62963a61844b22fcbbbae32ea8bc2f4326519
DenisWithOneN <denis.ursut@emanuel.com>
2023-04-25 11:31:35 +0300

    color code file + board color

--- board.js
+++ board.js
@@ -1,6 +1,6 @@
 function setup() {
     canvas = createCanvas(580, 500);
-    background(100);
+    background('#088395');
     let canvasX = (windowWidth - width) / 2;
     let canvasY = (windowHeight - height) / 2;
     canvas.position(canvasX, canvasY);
