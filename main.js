2f14cc708b6c037f1295d5d9112e3a467424054d
DenisWithOneN <denis.ursut@emanuel.com>
2023-05-10 16:30:13 +0300

    added player name labels, no design

--- main.js
+++ main.js
@@ -1,18 +1,22 @@
 function setup() {
-    canvas = createCanvas(950, 750);
-    let canvasX = (windowWidth - width) / 2;
-    let canvasY = (windowHeight - height) / 2;
-    canvas.position(canvasX, canvasY);
+  canvas = createCanvas(950, 750);
+  let canvasX = (windowWidth - width) / 2;
+  let canvasY = (windowHeight - height) / 2;
+  canvas.position(canvasX, canvasY);
 
-    initSquares();
+  initSquares();
+  playerNameInput = document.querySelector('#playerName');
 }
 
 function draw() {
-  background('#088395');
+  background("#088395");
   createSquares();
 
-
+  
 }
 
-
-
+function updatePlayerName() {
+  let playerName = playerNameInput.value;
+  let playerNameDisplay = document.getElementById("playerNameDisplay");
+  playerNameDisplay.innerHTML = playerName;
+}
