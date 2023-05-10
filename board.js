56c1937a10ad29cd488df62a69c6c90b222188f5
DenisWithOneN <denis.ursut@emanuel.com>
2023-05-10 13:19:05 +0300

    first try of init and create Squares

---
+++ board.js
@@ -0,0 +1,30 @@
+let columns = 9;
+let rows = 9;
+let spacing = 10;
+let totalBoardSquares = [];
+
+function initSquares() {
+
+    for(let i =0; i<=columns; i++)
+        for(let j = 0; j<=rows; j++){
+            let boardSquare = {
+                x: columns * i * spacing + 20,
+                y: rows * j * spacing + 20,
+                size: 50,
+                color: color("#00FFCA")
+            };
+
+            totalBoardSquares.push(boardSquare);
+
+        }
+}
+
+function createSquares() {
+
+    for(let i=0; i<totalBoardSquares.length; i ++){
+        let Squares = totalBoardSquares[i];
+        fill(boardSquare.color)
+        square(boardSquare.x, boardSquare.y, boardSquare.size);
+        
+    }
+}
