d28a8b79edbbb743d74550b6e8060509df672a74
DenisWithOneN <denis.ursut@emanuel.com>
2023-05-10 13:07:52 +0300

    added main.js in html instead of board.js

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 function setup() {
     canvas = createCanvas(580, 500);
-    background('#088395');
     let canvasX = (windowWidth - width) / 2;
     let canvasY = (windowHeight - height) / 2;
     canvas.position(canvasX, canvasY);
-  }
+}
+
+function draw() {
+
+  background('#088395');
+
+}
 
 
 
