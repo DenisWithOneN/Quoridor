9b0d7ea0f6b64538a6fb00b27f3d7b8f60bb272c
DenisWithOneN <denis.ursut@emanuel.ro>
2023-06-14 19:06:59 +0300

    created board constructor

--- boardClass.js
+++ boardClass.js
@@ -1,7 +1,13 @@
 class Board {
 
     constructor(columns, rows, spacing, boardMargin) {
-        
+        this.columns = columns;
+        this.rows = rows;
+        this.spacing = spacing;
+        this.boardMargin = boardMargin;
+        this.totalBoardSquares = [];
+        this.totalBoardRectangles = [];
+
     }
 
 
